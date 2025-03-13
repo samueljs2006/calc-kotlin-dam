86c438b13e3603b99cf8d00e331f88c2c5e2aa27
samueljs2006 <sjimsanc@g.educaand.es>
2025-03-13 18:29:56 +0100

    Funcionalidad dividir sin error

--- Calc.kt
+++ Calc.kt
@@ -7,6 +7,7 @@ fun main(args: Array) {
     showResult(operation, result)
 }
 
+
 fun parseArguments(args: Array): Triple {
     if (args.size != 3) {
         throw IllegalArgumentException("Número incorrecto de argumentos")
@@ -23,4 +24,10 @@ fun multiplica(num1: Int, num2: Int): Int {
 
 fun showResult(operation: String, result: Int) {
     println("El resultado de la operación \"$operation\" es: $result")
+}
+fun divide(num1: Int, num2: Int): Int {
+    if (num2 == 0) {
+        throw IllegalArgumentException("Error: No se puede dividir por cero")
+    }
+    return num1 / num2
 }
