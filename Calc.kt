2a59e17a162be772d925e5b02287908465b4926c
samueljs2006 <sjimsanc@g.educaand.es>
2025-03-13 18:56:11 +0100

    Funcionalidad sumar

--- Calc.kt
+++ Calc.kt
@@ -2,6 +2,7 @@ fun main(args: Array) {
     val (operation, num1, num2) = parseArguments(args)
     val result = when (operation) {
         "multiplica" -> multiplica(num1, num2)
+        "suma" -> suma(num1, num2)
         else -> throw IllegalArgumentException("Operación no soportada")
     }
     showResult(operation, result)
@@ -19,6 +20,9 @@ fun parseArguments(args: Array): Triple {
 
 fun multiplica(num1: Int, num2: Int): Int {
     return num1 * num2
+}
+fun suma(num1: Int, num2: Int): Int {
+    return num1 + num2
 }
 
 fun showResult(operation: String, result: Int) {
